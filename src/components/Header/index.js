acc23f2e8da4222b972699436975629edd62bc4e
Joseph Freeman <freemanjj1992@gmail.com>
2021-07-14 01:09:46 -0400

    first header

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Joe Freeman</h1>
+            <h2>Full Stack Developer</h2>
+        </header>
+    )
+}
+
+export default Header
