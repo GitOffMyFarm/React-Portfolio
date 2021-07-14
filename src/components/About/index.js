d8279978c5783cd643fc3156e7c6a857d3d9f14d
Joseph Freeman <freemanjj1992@gmail.com>
2021-07-14 01:19:24 -0400

    added about section

---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+          <p>
+          Full Stack web developer leveraging years of experience in client services and using business systems to create a natural user experience mixed with an efficient back-end. 
+          Recently earned a certificate in full-stack development from the University of Richmond developing new skills in Javascript, Node.js, React.js, among others. 
+          A detail-oriented and creative problem solver with a passion for programming results in a relentless approach to development demonstrated by my A average during the program. 
+          I have 100+ hours experience developing using Javascript, and recently was lead on a project where I did 100% of the front end javascript that involved search functions, API calls, and storage of specific items called up. 
+          I have not had a passion for any profession or skill in my life like I’ve had for programming, I will bring that passion, creativity, and thoroughness to your team and together we can create better web experiences on every level.
+          </p>  
+        </div>
+    )
+}
+
+export default About
