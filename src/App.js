58d73f66c51e099ac2efac73d917e5c14f72f45b
Joseph Freeman <freemanjj1992@gmail.com>
2021-07-14 11:59:18 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 import { useState } from 'react';
 
 const App = () => {
@@ -41,6 +42,7 @@ const App = () => {
       <Header />
       <About />
       <Projects projects={projects} />
+      <Contact />
     </div>
   );
 }
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,6 +3,11 @@ import React from 'react'
 const Contact = () => {
     return (
         <div>
+            <a class="list-group-item list-group-item-action" href="tel:804-615-6089">Phone: (804)615-6089</a>
+            <a class="list-group-item list-group-item-action" href="mailto:freemanjj1992@gmail.com"> Email: Freemanjj1992@gmail.com</a>
+            <a class="list-group-item list-group-item-action" href="https://www.linkedin.com/in/jjosephfreeman/" target="_blank" rel="noreferrer">Linkedin</a>
+            <a class="list-group-item list-group-item-action" href="https://github.com/GitOffMyFarm" target="_blank" rel="noreferrer">Github</a>
+            <a class="list-group-item list-group-item-action" href="/resume/Resume.pdf" target="_blank" rel="noreferrer">Resume</a>
             
         </div>
     )
